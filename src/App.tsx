d9fa8a631e18a95bfb3ff976d194f5625a50795a
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-16 22:38:03 +0300

    5 lesson - investigate map functionality, add key, index

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    const names = ["dimich", "Sveta", "Katia", "Victor", "Ignat", "Valera"]
+    const users = [{id:1, name: "dimich"}, {id:2, name: "Sveta"}, {id:3, name: "Katia"}, {id:4, name: "Victor"}, {id:5, name: "Ignat"}, {id:6, name: "Valera"}]
+
+    const libManifestPluginelements = users.map((n) => <li key={n.id}>{n.name}</li>)
+
+    return (
+        <div className="App">
 
 
+            <ul>
+                {libManifestPluginelements}
+            </ul>
 
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
