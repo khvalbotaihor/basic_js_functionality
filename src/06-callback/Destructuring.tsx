8cce4217042ef1cb57db92152b019085085e0391
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2020-10-17 14:15:54 +0300

    6 lesson -  destructuring assigment

--- src/06-callback/Destructuring.tsx
+++ src/06-callback/Destructuring.tsx
@@ -5,31 +5,32 @@ type PropsType = {
     title: string
     man: ManType
     food: Array<string>
-    car: {model:string}
+    car: { model: string }
 }
 
 function useDimichState(m: string) {
-    return[m, function () {
+    return [m, function () {
 
     }]
 }
+
 function useDimichState2(m: string) {
     return ({
-        message:m,
-    setMessage: function () {
-    }
-})
-
-export const ManComponent: React.FC<PropsType> = ({title, man, ...rest}) => {
-
-const [message, setMessage] = useState<string>("hello");
-
-    return <div>
-
-       <h1>{title}</h1>
-        <hr/>
-        <div>
-            {rest.car.model}
-        </div>
-    </div>
+        message: m,
+        setMessage: function () {
+        }
+    })
 }
+    export const ManComponent: React.FC<PropsType> = ({title, man, ...rest}) => {
+
+        const [message, setMessage] = useState<string>("hello");
+
+        return <div>
+
+            <h1>{title}</h1>
+            <hr/>
+            <div>
+                {rest.car.model}
+            </div>
+        </div>
+    }
--- src/06-callback/destructuring.test.ts
+++ src/06-callback/destructuring.test.ts
@@ -1,6 +1,7 @@
 
 type LessonType = {
     title:string
+    name?: string
 }
 type StreetType = {
     title: string
@@ -23,7 +24,7 @@ beforeEach(()=>{
     props = {
         name: "Dimich",
         age:32,
-        lessons:[{title:"1"},{title:"2"},{title:"3"}],
+        lessons:[{title:"1" },{title:"2"},{title:"3", name: "React"}],
         address:{
             street:{
                 title: "Nezaleznosty street"
@@ -43,7 +44,7 @@ test("", ()=>{
 
 
 expect(age).toBe(32);
-expect(lessons.length).toBe(2);
+expect(lessons.length).toBe(3);
 
 expect(title).toBe("Nezaleznosty street")
 })
@@ -53,11 +54,17 @@ test("",()=>{
     const l1 = props.lessons[0];
     const l2 = props.lessons[1];
 
-    const [, , ls2] = props.lessons
+    const [,ls2,...restLessons] = props.lessons
 
 
     expect(l1.title).toBe("1")
     expect(l2.title).toBe("2")
+
+    expect(restLessons.length).toBe(1)
+    expect(restLessons[0].title).toBe("3")
+    expect(restLessons[0].name).toBe("React")
+
+    expect(restLessons[0]).toStrictEqual({title:"3", name: "React"})
 
 
 })
